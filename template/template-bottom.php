b987a54f23c212d804e98076848e07f9bfa5197b
Biagio Arobba <barobba@gmail.com>
2012-08-18 07:22:57 -0600

    Adding URL to Facebook comments.
    Signed-off-by: Biagio Arobba <barobba@gmail.com>

--- template/template-bottom.php
+++ template/template-bottom.php
@@ -1,7 +1,8 @@
+<?php $comments_url = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>
 
         <div style="text-align: center" class="content-block">
           <h2>Please share your comments.</h2>
-				  <div class="fb-comments" data-num-posts="20" data-width="470" data-colorscheme="dark"></div>
+				  <div class="fb-comments" data-href="<?php echo $comments_url  ?>" data-num-posts="20" data-width="470" data-colorscheme="dark"></div>
 				</div>  	
 
   	  </div>
