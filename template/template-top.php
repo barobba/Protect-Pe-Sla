cc34b44234c193727a6f209aeb9f497319d5b904
Biagio Arobba <barobba@gmail.com>
2012-08-18 06:22:46 -0600

    Updated FB script.
    Signed-off-by: Biagio Arobba <barobba@gmail.com>

--- template/template-top.php
+++ template/template-top.php
@@ -35,14 +35,28 @@
   <body>
   
     <div id="fb-root"></div>
-    <script>(function(d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s); js.id = id;
-      js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=372923886111980";
-      fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));</script>
-      
+    <script>
+      window.fbAsyncInit = function() {
+        FB.init({
+          appId      : '372923886111980', // App ID
+          channelUrl : '//protectpesla.org/_fb-channel.html', // Channel File
+          status     : true, // check login status
+          cookie     : true, // enable cookies to allow the server to access the session
+          xfbml      : true  // parse XFBML
+        });
+    
+        // Additional initialization code here
+      };
+    
+      // Load the SDK Asynchronously
+      (function(d){
+         var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+         if (d.getElementById(id)) {return;}
+         js = d.createElement('script'); js.id = id; js.async = true;
+         js.src = "//connect.facebook.net/en_US/all.js";
+         ref.parentNode.insertBefore(js, ref);
+       }(document));
+    </script>      
   	<div id="page-wrapper" class="clearfix">
   	
   	  <div id="header-content" class="clearfix">
