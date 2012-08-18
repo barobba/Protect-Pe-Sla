21cd026dd7e2c415f52cd6a63494da2566c41cc7
Biagio Arobba <barobba@gmail.com>
2012-08-18 06:38:03 -0600

    Making presentation larger.
    Signed-off-by: Biagio Arobba <barobba@gmail.com>

--- why-pesla.php
+++ why-pesla.php
@@ -3,7 +3,7 @@
 
   <div style="text-align: center" class="content-block">
 	  <h2>Why Pe Sla is Important to Us</h2>
-	  <iframe src="http://www.slideshare.net/slideshow/embed_code/14005055" width="427" height="356" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC;border-width:1px 1px 0;margin-bottom:5px" allowfullscreen> </iframe>
+	  <iframe src="http://www.slideshare.net/slideshow/embed_code/14005055" width="597" height="486" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC;border-width:1px 1px 0;margin-bottom:5px" allowfullscreen> </iframe>
   </div>
   	  
   <div id="donation-request" class="content-block">
