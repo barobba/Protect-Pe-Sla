792f884d8ba9bc507006a5cef0d21f6031b5f0dd
Biagio Arobba <barobba@gmail.com>
2012-08-18 07:14:36 -0600

    Changed navigation link for homepage URL to '/'.

--- _navigation.php
+++ _navigation.php
@@ -1,5 +1,5 @@
 <ul class="nav">
-  <li><a href="index.php">Help Us</a></li>
+  <li><a href="/">Help Us</a></li>
   <li><a href="why-pesla.php">Why Pe Sla is Important to Us</a></li>
   <li><a href="news.php">In the News</a></li>
   <li><a href="news-release.php">NEWS RELEASE</a></li>
