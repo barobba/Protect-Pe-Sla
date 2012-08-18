f74f026dcb029aa30169252661bfa28cba0b23c4
Biagio Arobba <barobba@gmail.com>
2012-08-17 23:32:23 -0600

    Initial commit.
    Signed-off-by: Biagio Arobba <barobba@gmail.com>

---
+++ protect-pesla.php
@@ -0,0 +1,39 @@
+<html>
+  <head>
+	  <link rel="stylesheet" type="text/css" href="css/style.css" />
+  </head>
+  <body>
+  	<div id="page-wrapper" class="clearfix">
+  	
+  	  <div id="header-content" class="clearfix">
+  	    <div id="site-logo">
+    	    <img src="http://rosebudsiouxtribe-nsn.gov/templates/rst_default/Logo.png" />
+  	    </div>
+  	    <div id="site-name">
+	  	    <div class="heading"><a href="http://rosebudsiouxtribe-nsn.gov/"><img src="http://rosebudsiouxtribe-nsn.gov/templates/rst_default/Slogan.png" /></a></div>
+	  	    <div class="sub-heading">Protect Pe Sla</div>
+  	    </div>
+  	  </div>
+	    <div id="top-nav">
+	      <ul class="nav">
+	        <li><a href=".">In the News</a></li>
+	        <li><a href="protect-pesla.php">Help us Buy Back "Pe Sla"</a></li>
+	      </ul>
+	    </div>
+  	  <div id="body-content" class="clearfix">
+  	  
+  	    <div id="message" class="content-block">
+  	      <?php require_once '_message.php' ?>
+  	    </div>
+
+  	    <div id="donation-request" class="content-block">
+  	      <?php require_once '_donation-request.php' ?>
+  	    </div>
+    	  
+  	  </div>
+  	  
+  	</div>
+  	
+  	
+  </body>
+</html>
